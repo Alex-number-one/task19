42be10490b58140d81cd9967b46eb76c6103046f
Alex <aleksejtatukov098@gmail.com>
2023-06-30 18:22:21 +0300

    first task

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 9999 || num > 99999)
+{
+    Console.WriteLine("Please write an avaliable number");
+}
+else
+{
+    int num1 = num / 10000;
+    int num2 = num % 10;
+    int num3 = num / 1000; // don`t use
+    int num4 = num3 % 10;
+    int num5 = num % 100; // same
+    int num6 = num5 / 10;
+    if (num1 == num2 && num4 == num6)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+Console.WriteLine();
