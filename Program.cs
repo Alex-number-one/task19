5bcefaec2039e38e68924cb567c9d2e8d9e7e2ea
Alex <aleksejtatukov098@gmail.com>
2023-06-30 23:13:04 +0300

    second task

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int A = Convert.ToInt32(Console.ReadLine());
+int A1 = Convert.ToInt32(Console.ReadLine());
+int A2 = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int B1 = Convert.ToInt32(Console.ReadLine());
+int B2 = Convert.ToInt32(Console.ReadLine());
+double AB = Math.Sqrt(Math.Pow(B - A, 2) + Math.Pow(B1 - A1, 2) + Math.Pow(B2 - A2, 2));
+Console.WriteLine(AB);
