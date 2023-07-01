096622b6af8d11012ee0c5de3bbbb4e8bacdaab7
Alex <aleksejtatukov098@gmail.com>
2023-07-01 08:44:20 +0300

    task23

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+
+int index = 1;
+while (index <= n)
+{
+int res = index;
+double result = Math.Pow(res, 3);
+index++;
+Console.WriteLine(result);
+}
+
